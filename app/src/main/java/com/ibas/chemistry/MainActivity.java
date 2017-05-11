5175e2862218204b9265d0245b45fd4944b6026c
Ardi Baskoro <mistersemar.ab@gmail.com>
2017-05-11 19:57:32 +0700

    Add Tabs, Add Fragment

--- app/src/main/java/com/ibas/chemistry/MainActivity.java
+++ app/src/main/java/com/ibas/chemistry/MainActivity.java
@@ -1,7 +1,17 @@
 package com.ibas.chemistry;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new TileFragment(), "Kalkulator");
+        adapter.addFragment(new TileFragment(), "Kosong");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager){
+            super(manager);
+        }
+
+        public void addFragment(Fragment fragment, String tilte){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(tilte);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
     }
 }
--- app/src/main/java/com/ibas/chemistry/TileFragment.java
+++ app/src/main/java/com/ibas/chemistry/TileFragment.java
@@ -1,8 +1,90 @@
 package com.ibas.chemistry;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by Ibas on 11/05/2017.
  */
 
-public class TileFragment {
+public class TileFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView)inflater.inflate(
+                R.layout.recycle_view, container, false
+        );
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        // Set padding for Tiles
+        int titlePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+        recyclerView.setPadding(titlePadding, titlePadding, titlePadding, titlePadding);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView picture;
+        public TextView name;
+        public ViewHolder(LayoutInflater inflater, ViewGroup viewGroup){
+            super(inflater.inflate(R.layout.item_tile, viewGroup, false));
+            picture = (ImageView)itemView.findViewById(R.id.tile_picture);
+            name = (TextView)itemView.findViewById(R.id.tile_text);
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder>{
+        private final String[] mTitle;
+        private final Drawable[] mPicture;
+
+        public ContentAdapter(Context context){
+            Resources resources = context.getResources();
+            mTitle = resources.getStringArray(R.array.places);
+            TypedArray typedArray = resources.obtainTypedArray(R.array.places_picture);
+            mPicture = new Drawable[typedArray.length()];
+            for (int i = 0; i < mPicture.length; i++){
+                mPicture[i] = typedArray.getDrawable(i);
+            }
+            typedArray.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(
+                    parent.getContext()
+            ), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.picture.setImageDrawable(mPicture[position % mPicture.length]);
+            holder.name.setText(mTitle[position % mTitle.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPicture.length;
+        }
+    }
 }
